3264ee0183e4a9ab16f6a9fec2048409230dbc72
yakoobkhedri <yakkobkhedrii@gmail.com>
2024-10-29 17:04:53 +0330

    commit message

---
+++ assets/js/scripts.js
@@ -0,0 +1,82 @@
+// swiper
+var banner = new Swiper(".banner", {
+  slidesPerView: 1,
+  spaceBetween: 15,
+  autoplay: true,
+  navigation: {
+    nextEl: ".swiper-button-next2",
+    prevEl: ".swiper-button-prev2",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  }
+});
+var portfolio = new Swiper(".portfolio", {
+  slidesPerView: 2,
+  spaceBetween: 15,
+  autoplay: true,
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 25,
+    },
+    992: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 5,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+var forms = new Swiper(".forms", {
+  slidesPerView: 2,
+  spaceBetween: 20,
+  autoplay: true,
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 25,
+    },
+    992: {
+      slidesPerView: 4,
+    },
+    1200: {
+      slidesPerView: 6,
+    },
+  }
+});
+var customers = new Swiper(".customers", {
+  slidesPerView: 2,
+  spaceBetween: 25,
+  autoplay: true,
+  breakpoints: {
+    400: {
+      slidesPerView: 3,
+    },
+    576: {
+      slidesPerView: 4,
+    },
+    768: {
+      slidesPerView: 6,
+    },
+    992: {
+      slidesPerView: 8,
+    },
+    1200: {
+      slidesPerView: 10,
+    },
+  }
+});
+
+// timer
